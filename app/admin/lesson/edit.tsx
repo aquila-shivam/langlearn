d702f4a0f0984ee75b45e50dc5868fd1cabe0b88
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2024-04-18 06:41:36 +0530

    admin

---
+++ app/admin/lesson/edit.tsx
@@ -0,0 +1,14 @@
+import { Edit,SimpleForm, required, TextInput, ReferenceInput, NumberInput } from "react-admin";
+
+export const LessonEdit = () =>{
+
+    return (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="title" validate = {[required()]} label="Title"/>
+            <ReferenceInput source = "unitId" reference="units"/>
+            <NumberInput source="order" validate = {[required()]} label="Order"/>
+        </SimpleForm>
+    </Edit>
+    )
+}
---
+++ app/admin/unit/edit.tsx
@@ -0,0 +1,16 @@
+import { SimpleForm, required, TextInput,Edit, NumberInput, ReferenceInput } from "react-admin";
+
+export const UnitEdit = () =>{
+
+    return (
+    <Edit>
+        <SimpleForm>
+            <NumberInput source="id" validate = {[required()]} label="Id"/>
+            <TextInput source="title" validate = {[required()]} label="Title"/>
+            <TextInput source = "description" validate = {[required()]} label="Description"/>
+            <ReferenceInput source="courseId" reference="courses"/>
+            <NumberInput source="order" validate = {[required()]} label="Order"/>
+        </SimpleForm>
+    </Edit>
+    )
+}
